618e91bae00acf08ba6a855b27fcb41b77cefdbb
Anam9820 <khan.anam.19bsc203@gmail.com>
2022-09-27 15:42:32 +0530

    upload new file

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function validate(){
+    var name=document.getElementById('name').value;
+    var username=document.getElementById('username').value;
+    var number=document.getElementById('number').value;
+    var email=document.getElementById('email').value;
+    var password=document.getElementById('password').value;
 
+    console.log(name);
+
+    if(name==null || name==""){
+        document.getElementById('fname').innerHTML="Please enter your full name";
+        return false;
+    }
+    else{
+        document.getElementById('fname').innerHTML="";
+        return false;
+    }
+
+}
 
 
